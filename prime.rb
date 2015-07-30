f59821184631dc58e2e117be103f3d8f9b1a0079
jessicashannon <jessdahling@gmail.com>
2015-07-30 15:13:45 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(number)
+  if number <= 2
+    return true
+  end
+  x = 2
+  while x < number
+    if number % x == 0
+      return false
+    else
+      x += 1
+    end
+  end
+  return true
+end
